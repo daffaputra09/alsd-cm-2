631082bed84e0e3e3404da72a956a5ebb2266f81
daffaputra09 <daffaputra863@gmail.com>
2025-06-05 15:05:11 +0700

    feat: menambahkan class BBM, Kendaraan, dan TransaksiPengisian

---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    public Kendaraan kendaraan;
+    public Node next;
+
+    public Node(Kendaraan kendaraan) {
+        this.kendaraan = kendaraan;
+        this.next = null;
+    }
+}
